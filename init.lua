0d3fd887cd2cccc28086afdb1fbd5c79d908a08b
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2023-07-29 20:19:30 -0400

    Edit keybindings

--- init.lua
+++ init.lua
@@ -94,11 +94,11 @@ return {
       { desc = "Toggle Neotree", noremap = true, silent = true }
     )
     -- Move lines and blocks
-    map("n", "<A-k>", ":m .+1<CR>==", { desc = "Move line up", noremap = true, silent = true })
-    map("n", "<A-j>", ":m .-2<CR>==", { desc = "Move line down", noremap = true, silent = true })
+    map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down", noremap = true, silent = true })
+    map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up", noremap = true, silent = true })
     map("n", "<A-l>", "yyp", { desc = "Duplicate line", noremap = true, silent = true })
-    map("v", "<A-k>", ":m '>+1<CR>gv=gv", { desc = "Move block up", noremap = true, silent = true })
-    map("v", "<A-j>", ":m '<-2<CR>gv=gv", { desc = "Move block down", noremap = true, silent = true })
+    map("v", "<A-k>", ":m '>+1<CR>gv=gv", { desc = "Move block down", noremap = true, silent = true })
+    map("v", "<A-j>", ":m '<-2<CR>gv=gv", { desc = "Move block up", noremap = true, silent = true })
     map("v", "<A-l>", "Vy0P", { desc = "Duplicate block", noremap = true, silent = true })
   end,
 }
