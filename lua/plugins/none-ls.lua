afda9fa281525e56cecbf975bda0c5f7f236ff59
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-07-24 23:27:53 -0400

    Fix AutoSave/AutoFormat/Undo issue

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,10 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+-- fix AutoSave/Autoformat/Undo Bug
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/879#issuecomment-1133925084
+-- [ 1 / 2 ]
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- Customize None-ls sources
 
 ---@type LazySpec
@@ -18,6 +23,32 @@ return {
       null_ls.builtins.formatting.prettierd,
       -- null_ls.builtins.formatting.autopep8,
     }
+    -- fix AutoSave/Autoformat/Undo Bug
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/879#issuecomment-1133925084
+    -- [ 2 / 2 ]
+    config.on_attach = function(client, bufnr)
+      if client.supports_method "textDocument/formatting" then
+        vim.api.nvim_buf_create_user_command(bufnr, "LspFormatting", function()
+          -- or vim.lsp.buf.formatting(bufnr) on 0.8
+          vim.lsp.buf.formatting_sync()
+        end, {})
+
+        -- you can leave this out if your on_attach is unique to null-ls,
+        -- but if you share it with multiple servers, you'll want to keep it
+        vim.api.nvim_clear_autocmds {
+          group = augroup,
+          buffer = bufnr,
+        }
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          command = "undojoin | LspFormatting",
+        })
+      end
+    end
+    --
+
     return config -- return final config table
   end,
 }
