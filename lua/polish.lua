e59dd43828eb086ac6d5add2ba40197341cc7736
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-08-02 09:57:17 -0400

    add spelling and cmp support

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,4 +1,4 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
@@ -16,3 +16,12 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+
+-- Enable & customise spell checking
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+vim.api.nvim_set_hl(
+  0, -- global highlight group
+  "SpellBad",
+  { fg = "PaleGoldenRod", bg = "darkred", underline = true }
+)
