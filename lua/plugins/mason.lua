e42d4c184c21e9c3791bf1a1822ff41221155b92
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-09-23 08:54:30 -0400

    update typescript-language-server support & AstroUpdate

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,7 +13,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
         "rust_analyzer",
-        "tsserver",
+        "ts_ls",
         "clangd",
         "html",
         "htmx",
