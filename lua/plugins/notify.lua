e058c52cd28b99adf6b2fefdb86d7c8cc088ed5f
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-10-13 02:31:13 -0400

    add notify.lya

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,12 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "rcarriga/nvim-notify",
+    config = {
+      render = "compact",
+      -- stages = "slide",
+      timeout = 1000,
+    },
+  },
+}
