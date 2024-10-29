14b510a57f14f1170b822a1564af0c3d440a0fdb
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-10-29 13:22:51 -0400

    AstroUpdate

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,6 +13,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
         "rust_analyzer",
+        "svelte",
         "ts_ls",
         "clangd",
         "html",
